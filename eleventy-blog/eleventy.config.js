b47d39d0813eb5959730d2a74c0d94c7df5112e6
Chris Eadie <caeadie@hotmail.com>
2025-01-26 19:05:46 +0000

    updates to website styles

--- eleventy-blog/eleventy.config.js
+++ eleventy-blog/eleventy.config.js
@@ -1,24 +1,27 @@
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
-export default function(eleventyConfig){
+export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./styles/main.css");
     eleventyConfig.addWatchTarget("./styles/main.css")
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("./javascript/main.js");
+    eleventyConfig.addWatchTarget("./javascript/main.js")
 
-    eleventyConfig.addPlugin(feedPlugin,{
-        type:"rss",
-        outputPath:"/feed.xml",
-        collection:{
-            name:"post",
-            limit:0
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "rss",
+        outputPath: "/feed.xml",
+        collection: {
+            name: "post",
+            limit: 0
         },
-        metadata:{
+        metadata: {
             language: "en",
-			title: "Chris Eadie Blog",
-			subtitle: "This is a longer description about your blog.",
-			base: "https://chriseadie.com/",
-			author: {
-				name: "Chris Eadie",
-			}
+            title: "Chris Eadie Blog",
+            subtitle: "This is a longer description about your blog.",
+            base: "https://chriseadie.com/",
+            author: {
+                name: "Chris Eadie",
+            }
         }
     })
 }
